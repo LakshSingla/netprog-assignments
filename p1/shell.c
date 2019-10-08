593ea8f06018352e20ad81e4d83a852bcffba1f2
Laksh Singla <lakshsingla@gmail.com>
2019-10-09 01:50:35 +0530

    trying to change fg and bg process groups

--- p1/shell.c
+++ p1/shell.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
 
 
 #include "constants.h"
@@ -25,7 +28,71 @@ int main() {
 			continue;
 		}
 
+		pid_t child_executer = fork();
+		int child_executer_status;
 
+		int p_sync_fg[2];
+		pipe(p_sync_fg);
+
+		if(child_executer < 0) {
+			//error	
+		}
+
+		else if(child_executer > 0) {
+			close(p_sync_fg[0]);
+			printf("waiting for child\n");
+			printf("Parent pgid: %d\n", getpgid(0));
+
+			if(setpgid(child_executer, child_executer) == -1) {
+				printf("Unable to create a new process group. Exiting command...\n");
+				exit(0);
+			}
+
+			printf("Current terminal controlling process: %d\n", tcgetpgrp(STDIN_FILENO));
+			if(tcsetpgrp(STDIN_FILENO, child_executer) == -1) {
+				printf("Unable to set process group as foreground. Exiting command...\n");
+				perror("se: ");
+				exit(0);
+			}
+			write(p_sync_fg[1], "T", 1);
+			printf("askldfjlsd\n");
+			waitpid(child_executer, &child_executer_status, WUNTRACED);	
+			printf("here\n");
+			//setpgid(0, child_executer);
+			signal(SIGTTOU, SIG_IGN);
+			tcsetpgrp(STDIN_FILENO, getpid());
+			signal(SIGTTOU, SIG_DFL);
+			printf("waited for child\n");
+			close(p_sync_fg[1]);
+		}
+
+		else {
+			close(p_sync_fg[1]);
+			char buf_sync_fg[3];
+			read(p_sync_fg[0], buf_sync_fg, 1);
+			printf("here1\n");
+			printf("child pgid1: %d\n", getpgid(0));
+			int stdin_pgid = tcgetpgrp(STDIN_FILENO);
+			printf("Current terminal controlling process: %d\n", stdin_pgid);
+
+			setpgid(0, child_executer);
+			/*if(setpgid(0, 0) == -1) {
+				printf("Unable to create a new process group. Exiting command...\n");
+				exit(0);
+			}
+			printf("child pgid2: %d\n", getpgid(0));
+			signal(SIGTTOU, SIG_IGN);
+			if(tcsetpgrp(STDIN_FILENO, stdin_pgid) == -1) {
+				printf("Unable to set process group as foreground. Exiting command...\n");
+				perror("se: ");
+				exit(0);
+			}
+			signal(SIGTTOU, SIG_DFL);*/
+			printf("asdf Current terminal controlling process: %d\n", tcgetpgrp(STDIN_FILENO));
+			//tcsetpgrp(STDIN_FILENO, stdin_pgid);
+			printf("execing: %s\n", canonical_path);
+			execl(canonical_path, canonical_path, (char*)0);
+		}
 
 
 		printf("%s\n", canonical_path);
