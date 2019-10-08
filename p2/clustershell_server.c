1063ceca1f61578c8ee6bbad00b9303e23756391
Laksh Singla <lakshsingla@gmail.com>
2019-10-08 16:39:06 +0530

    nodes implemented

--- p2/clustershell_server.c
+++ p2/clustershell_server.c
@@ -44,83 +44,103 @@ int main(int argc, char **argv) {
 			close(serv_sock);
 
 			while (true) {
-			char cmd_buf[__MAX_CMD_SIZE__];
-			int n = read(clnt_sock, cmd_buf, __MAX_CMD_SIZE__);
-			if(n < 0) { break; exit(-1); }
-			cmd_buf[n] = '\0';
-			printf("Received command: %s\n", cmd_buf);
+				char cmd_buf[__MAX_CMD_SIZE__];
+				int n = read(clnt_sock, cmd_buf, __MAX_CMD_SIZE__);
+				if(n < 0) { break; exit(-1); }
+				cmd_buf[n] = '\0';
+				printf("Received command: %s\n", cmd_buf);
 
-			PARSE_OBJ pcmd = parse(cmd_buf);
-			int cmd_it = 0;
-			char input_buf[__MAX_OUT_SIZE__ + 1] = {0};
-			int input_buf_size = 0;
-			while(!PARSE_EMPTY(pcmd, cmd_it)) {
-				char *cl = PARSE_GET_KEY(pcmd, cmd_it);
-				char *in_cmd = PARSE_GET_VAL(pcmd, cmd_it);
 
-				if(cl[1] == '*') {
-					printf("Handling wild\n");
-					char **cur_ip = config_ips;
-					char concat_resp[__MAX_OUT_SIZE__+1];
-					int concat_offset = 0;
-					while(*cur_ip) {
-						printf("XXX\n");
-						int con_fd = clnt_side_setup(*cur_ip, __CLIENT_PORT__);				
-						char response[__MAX_CMD_SIZE__ + 1 + __MAX_OUT_SIZE__ + 1] = {0};	
+				if(strcmp(cmd_buf, __NODES_CMD__) == 0) {
+					printf("Nodes command recieved\n");	
+					FILE *fp = fopen(__CONFIG_FILE__, "r");
+					if(fseek(fp, 0L, SEEK_END) != 0) {
+					
+					}
+					long buf_size = ftell(fp);
+					if(buf_size == -1) {}
+					char *src = malloc(sizeof(char) * (buf_size+1));
+
+					if(fseek(fp, 0L, SEEK_SET) != 0) {}
+
+					size_t new_len = fread(src, sizeof(char), buf_size, fp);
+					src[new_len] = 0;
+					++new_len;
+					write(clnt_sock, src, new_len);
+					continue;
+				}
+
+				PARSE_OBJ pcmd = parse(cmd_buf);
+				int cmd_it = 0;
+				char input_buf[__MAX_OUT_SIZE__ + 1] = {0};
+				int input_buf_size = 0;
+				while(!PARSE_EMPTY(pcmd, cmd_it)) {
+					char *cl = PARSE_GET_KEY(pcmd, cmd_it);
+					char *in_cmd = PARSE_GET_VAL(pcmd, cmd_it);
+
+					if(cl[1] == '*') {
+						printf("Handling wild\n");
+						char **cur_ip = config_ips;
+						char concat_resp[__MAX_OUT_SIZE__+1];
+						int concat_offset = 0;
+						while(*cur_ip) {
+							printf("XXX\n");
+							int con_fd = clnt_side_setup(*cur_ip, __CLIENT_PORT__);				
+							char response[__MAX_CMD_SIZE__ + 1 + __MAX_OUT_SIZE__ + 1] = {0};	
+							strcpy(response, in_cmd);
+							strcpy(response+strlen(in_cmd)+1, input_buf);
+							int response_size = strlen(in_cmd)+1+strlen(input_buf)+1;
+							int nbytes = write(con_fd, response, response_size);
+							if(nbytes != response_size) {
+								printf("Error in writing. Exiting...\n");	
+							}
+							char tmp_buf[__MAX_OUT_SIZE__ + 1];
+							int tmp_buf_size;
+							tmp_buf_size = read(con_fd, tmp_buf, __MAX_OUT_SIZE__+1);
+							tmp_buf[tmp_buf_size] = 0;
+							printf("%s\n", tmp_buf);
+							strcpy(concat_resp+concat_offset, tmp_buf);
+							concat_offset += tmp_buf_size;
+							concat_resp[concat_offset] = '\n';
+							concat_offset += 1;
+							close(con_fd);
+							++cur_ip;	
+						}
+						concat_resp[concat_offset] = 0;
+						input_buf_size = concat_offset+1;
+						strcpy(input_buf, concat_resp);
+					}
+
+					else {
+						int con_fd;				
+						if(cl[0] != '-') {
+							int cl_no = atoi(cl + 1);	
+							con_fd = 	clnt_side_setup(config_ips[cl_no-1], __CLIENT_PORT__);
+						}
+						else {
+							con_fd = clnt_side_setup(inet_ntoa(clnt_addr.sin_addr), __CLIENT_PORT__);	
+						}
+						char response[__MAX_CMD_SIZE__ + 1 + __MAX_OUT_SIZE__ + 1] = {0};
 						strcpy(response, in_cmd);
 						strcpy(response+strlen(in_cmd)+1, input_buf);
 						int response_size = strlen(in_cmd)+1+strlen(input_buf)+1;
+
+						printf("Sending cmd: %s\n", response);
 						int nbytes = write(con_fd, response, response_size);
 						if(nbytes != response_size) {
 							printf("Error in writing. Exiting...\n");	
 						}
-						char tmp_buf[__MAX_OUT_SIZE__ + 1];
-						int tmp_buf_size;
-						tmp_buf_size = read(con_fd, tmp_buf, __MAX_OUT_SIZE__+1);
-						tmp_buf[tmp_buf_size] = 0;
-						printf("%s\n", tmp_buf);
-						strcpy(concat_resp+concat_offset, tmp_buf);
-						concat_offset += tmp_buf_size;
-						concat_resp[concat_offset] = '\n';
-						concat_offset += 1;
+						input_buf_size = read(con_fd, input_buf, __MAX_OUT_SIZE__+1);
+						printf("Pipe out: %s\n", input_buf);
 						close(con_fd);
-						++cur_ip;	
 					}
-					concat_resp[concat_offset] = 0;
-					input_buf_size = concat_offset+1;
-					strcpy(input_buf, concat_resp);
+					++cmd_it;
 				}
-
-				else {
-					int con_fd;				
-					if(cl[0] != '-') {
-						int cl_no = atoi(cl + 1);	
-						con_fd = 	clnt_side_setup(config_ips[cl_no-1], __CLIENT_PORT__);
-					}
-					else {
-						con_fd = clnt_side_setup(inet_ntoa(clnt_addr.sin_addr), __CLIENT_PORT__);	
-					}
-					char response[__MAX_CMD_SIZE__ + 1 + __MAX_OUT_SIZE__ + 1] = {0};
-					strcpy(response, in_cmd);
-					strcpy(response+strlen(in_cmd)+1, input_buf);
-					int response_size = strlen(in_cmd)+1+strlen(input_buf)+1;
-
-					printf("Sending cmd: %s\n", response);
-					int nbytes = write(con_fd, response, response_size);
-					if(nbytes != response_size) {
-						printf("Error in writing. Exiting...\n");	
-					}
-					input_buf_size = read(con_fd, input_buf, __MAX_OUT_SIZE__+1);
-					printf("Pipe out: %s\n", input_buf);
-					close(con_fd);
-				}
-				++cmd_it;
-			}
-			input_buf[input_buf_size] = 0;
-			++input_buf_size;
-			printf("Received output: %s\n", input_buf);
-			write(clnt_sock, input_buf, input_buf_size);
-			printf("reached here\n");
+				input_buf[input_buf_size] = 0;
+				++input_buf_size;
+				printf("Received output: %s\n", input_buf);
+				write(clnt_sock, input_buf, input_buf_size);
+				printf("reached here\n");
 			}
 			close(clnt_sock);
 		}
--- p2/constants.h
+++ p2/constants.h
@@ -8,7 +8,8 @@
 #define __MAX_OUT_SIZE__ 		1024
 #define __CLIENT_PORT__ 		4000
 #define __SERVER_PORT__ 		5000
-#define __CONFIG_FILE__ 		"CONFIG"
-#define __EXIT_CMD__ "exit"
+#define __CONFIG_FILE__ 		"./CONFIG"
+#define __EXIT_CMD__ 				"exit"
+#define __NODES_CMD__ 			"nodes"
 
 #endif
