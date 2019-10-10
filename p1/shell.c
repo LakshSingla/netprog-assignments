7ccc9348789da3474abc9817d7d62467669ad15f
kunal-mohta <kunalmohta1818@gmail.com>
2019-10-11 04:34:16 +0530

    empty cmd not allowd

--- p1/shell.c
+++ p1/shell.c
@@ -12,13 +12,16 @@
 #include "proc_management.h"
 
 int main() {
+	printf("Shell started!\n\n");
 	while(true) {
 		printf(__PROMPT__);
 
 		size_t cmd_size = __MAX_CMD_SIZE__ + 1;
 		char *cmd_buf =	malloc(sizeof(char) * cmd_size);
 		ssize_t cmd_size_act = getline(&cmd_buf, &cmd_size, stdin);
-		if(cmd_size_act == -1 || cmd_size_act == 0) { /* error */ }
+		if(cmd_size_act == -1 || cmd_size_act == 0 || (cmd_size_act == 1 && cmd_buf[0] == '\n')) {
+			continue;
+		}
 		cmd_buf[cmd_size_act-1] = 0;
 		bool is_bg_cmd = false;
 
